6bd026a588c6ddc17f32c504c135842891dee1ca
Spencer Bouse <Orangesrb@gmail.com>
2018-04-04 23:05:43 -0400

    github link

--- src/projects-data.js
+++ src/projects-data.js
@@ -7,25 +7,29 @@ const projects = {
   proj1:{
     name: 'Quest Finder',
     image: proj1,
-    desc: 'Quest Finder is a Dungeon and Dragons webapp where Dungeon Masters are matched with Dungeons and Dragons players. Made with AngularJs.'
+    desc: 'Quest Finder is a Dungeon and Dragons webapp where Dungeon Masters are matched with Dungeons and Dragons players. Made with AngularJs.',
+    link: 'https://github.com/SpencerBouse/Quest-Finder'
   },
 
   proj2:{
     name: 'Yohra New Tab',
     image: proj2,
-    desc: 'Google Chrome New Tab extension based off Nier Automata. Made with VueJs.'
+    desc: 'Google Chrome New Tab extension based off Nier Automata. Made with VueJs.',
+    link: 'https://github.com/pschoen325/yorha_new_tab'
   },
 
   proj3:{
     name: 'Todo App',
     image: proj3,
-    desc: 'Basic Todo App. Made with AngularJs'
+    desc: 'Basic Todo App. Made with AngularJs',
+    link: 'https://github.com/SpencerBouse/ToDoodle'
   },
 
   proj4:{
     name: 'Instagram Clone',
     image: proj4,
-    desc: 'Old Instagram style clone. Made with AngularJs'
+    desc: 'Old Instagram style clone. Made with AngularJs',
+    link: 'https://github.com/SpencerBouse/InstaClone'
   },
 }
 
